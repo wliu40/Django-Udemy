7b04efedebb536c4564a145c83fdd568fba0da9e
wliu40 <wliu0828@gmail.com>
2024-04-10 21:14:34 -0400

    Refactor views.py to improve code readability and add comments

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -66,7 +66,16 @@ my_posts = [
         """
     }
 ]
-# Create your views here.
+
+
+# pay attention to those functions and the parameters they take
+# the index page will show the first 3 posts, in the index.html file you can see we had a for loop that iterated over the posts
+# and displayed them, we passed the posts as a parameter to the render function
+
+# the posts page will show all the posts, we passed all the posts to the render function
+# inside the all-posts.html file we had a for loop that iterated over the posts and displayed them
+
+# the post detail page will show the details of a single post, we passed the identified post to the render function
 
 def starting_page(request):
     return render(request, 'blog/index.html', {'posts': my_posts[:3]})
